fa605033cd4dffea9d27e95cb8485d349630a5ce
krviknesh10-eng <krviknesh10@gmail.com>
2026-08-21 13:04:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,8 @@ def webhook():
                 base_url="https://api.groq.com/openai/v1"
             )
 
-            MODEL = "llama-3.3-70b-versatile"
+            #MODEL = "llama-3.3-70b-versatile"
+            MODEL = "openai/gpt-oss-20b"
             response = client.responses.create(
                 model=MODEL,
                 input=message
