22d5b265ef7471f7969d84f2ad6a9ae02db40f5d
krviknesh10-eng <krviknesh10@gmail.com>
2026-08-21 13:08:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def webhook():
             message = text[len("/movie"):].strip()
             send_message(
                 chat_id,
-                search_movies("karuppu")
+                search_movies(message)
             )
 
         # Normal message
